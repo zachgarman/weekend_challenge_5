9297ca419887c2347365f67a115f88dbf7e0dc36
Zach Garman <zachgarman@gmail.com>
2016-10-21 22:16:57 -0500

    server and routes operational

---
+++ README.md
@@ -0,0 +1,9 @@
+# The Great Gif Collection
+
+This project aims to allow a user to search for random or specific gifs
+via the Giphy API and save the gif url's and comments to a database.  It
+is a full-stack project incorporating aspects of everything we have learned
+at Prime in the first 5 weeks of class.
+
+### Author
+Zach Garman
--- routes/favoritesRouter.js
+++ routes/favoritesRouter.js
@@ -30,4 +30,34 @@ router.get('/', function(req, res) {
   });
 });
 
+router.post('/', function(req, res) {
+  pool.connect(function (err, client, done) {
+    try {
+      if (err) {
+        console.log('Error connnecting to the DB', err);
+        res.sendStatus(500);
+        return;
+      }
+
+      var url = req.body.url;
+      var comment = req.body.comment;
+      var category = req.body.category;
+
+      client.query('INSERT INTO favorite_gifs (url, comment, category) VALUES ($1, $2, $3) RETURNING*;',
+                   [url, comment, category],
+                   function(err, result) {
+                    if (err) {
+                      console.log('Error querying the DB', err);
+                      res.sendStatus(500);
+                      return;
+                    }
+        res.send(result.rows);
+      });
+    } finally {
+      done();
+    }
+  });
+});
+
+
 module.exports = router;
